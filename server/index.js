68c1928a21694aa6e2a234448ad5e60729ad55a5
Suraj Kunwor <dsurajkunwor101@gmail.com>
2026-05-14 22:57:52 +0545

    docs(server): add jsdoc to http server entry

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const { Server } = require('socket.io');
 const app = require('./src/app');
 
 const PORT = process.env.PORT || 5000;
+
+/**
+ * HTTP Server Instance
+ */
 const server = http.createServer(app);
 
 const io = new Server(server, {
