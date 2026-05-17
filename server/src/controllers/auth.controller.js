bd59cf412f488e5aa0199b44f4dde7135c5e925d
Suraj Kunwor <dsurajkunwor101@gmail.com>
2026-05-17 22:35:40 +0545

    fix(backend): correct PrismaClient initialization in auth controller

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 const { PrismaClient } = require('@prisma/client');
 const generateToken = require('../utils/generateToken');
 
-const prisma = new PrismaClient({ accelerateUrl: process.env.DATABASE_URL });
+const prisma = new PrismaClient();
 
 /**
  * Register a new user
