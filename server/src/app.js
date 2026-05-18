ef884618fb13944168d8541cdb5b57b81a8a96f4
Suraj Kunwor <dsurajkunwor101@gmail.com>
2026-05-18 22:52:30 +0545

    fix(server): configure CORS with origin and credentials

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,10 @@ const scoreRoutes = require('./routes/score.routes');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(helmet());
 app.use(compression());
 app.use(express.json());
