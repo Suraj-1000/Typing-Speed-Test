2753d8b60f36aba32cde6ffcbd1ee8011fbbcfa0
Suraj Kunwor <dsurajkunwor101@gmail.com>
2026-05-19 23:25:25 +0545

    docs(client): add comment to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+// Import main App component
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
