91c2d77367a79fbf25c0e1eeca0e7cc5ce62d9b1
Suraj Kunwor <dsurajkunwor101@gmail.com>
2026-05-19 23:23:20 +0545

    fix(client): remove unused err variable in Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -26,7 +26,7 @@ const Register = () => {
     try {
       await register(username, email, password);
       navigate('/');
-    } catch (err) {
+    } catch (_) {
       // Error is handled by store
     }
   };
