bd15bd2f36d433564ae922586e3d70eb9c52d06d
Suraj Kunwor <dsurajkunwor101@gmail.com>
2026-05-19 23:26:23 +0545

    docs(server): add comment to score routes

--- server/src/routes/score.routes.js
+++ server/src/routes/score.routes.js
@@ -3,6 +3,7 @@ const { saveScore, getHistory } = require('../controllers/score.controller');
 const protectRoute = require('../middleware/protectRoute');
 
 const router = express.Router();
+// Score related routes
 
 router.post('/', protectRoute, saveScore);
 router.get('/history', protectRoute, getHistory);
