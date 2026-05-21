6e9c9303e80419e300e4ea13313314c4b0617948
Suraj Kunwor <dsurajkunwor101@gmail.com>
2026-05-21 18:24:02 +0545

    test(e2e): restrict Playwright projects to chromium for stability

--- client/playwright.config.js
+++ client/playwright.config.js
@@ -34,14 +34,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
   ],
 
   /* Run local dev server before starting the tests */
